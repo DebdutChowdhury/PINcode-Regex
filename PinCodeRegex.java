77f5d5ba9924dde7b0e67e7a42a528b0c90926f2
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-30 22:03:03 +0530

    Added: restrict in end of pincode

--- PinCodeRegex.java
+++ PinCodeRegex.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class PinCodeRegex {
     public static boolean IsPinCodeMatch(String pin){
-        Pattern pattern = Pattern.compile("^[0-9]{6}");
+        Pattern pattern = Pattern.compile("^[0-9]{6}");    // my pincode regex here 
         Matcher matcher = pattern.matcher(pin);
         boolean result = matcher.matches();
 //        System.out.println(result);
@@ -12,10 +12,13 @@ public class PinCodeRegex {
     public static void main(String[] args) {
         System.out.println("Matcing pincode using regex:");
         System.out.println("---------------------------");
-        String p1 = "400088";
+        String p1 = "400088";                                          // 6 digit pincode number
         System.out.println(p1 + " ==> " +IsPinCodeMatch(p1));
 
-        String p2 = "A400088";
+        String p2 = "A400088";                                        // in the beginning of pincode letter or syamol not allow
         System.out.println(p2 + " ==> " +IsPinCodeMatch(p2));
+
+	 String p3 = "400088B";					     // in the end of pincode letter or symbol not allow 
+        System.out.println(p3+ " ==> "+IsPinCodeMatch(p3));
     }
 }
