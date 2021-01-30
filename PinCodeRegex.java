e17de96242cdc7cdde2cc439b4583c2b59892024
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-30 21:49:16 +0530

    Added: letter and symbol restrict

--- PinCodeRegex.java
+++ PinCodeRegex.java
@@ -2,12 +2,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PinCodeRegex {
-    public static void main(String[] args) {
-        System.out.println("Matcing pincode using regex");
-		  System.out.println("---------------------------");
+    public static boolean IsPinCodeMatch(String pin){
         Pattern pattern = Pattern.compile("^[0-9]{6}");
-        Matcher matcher = pattern.matcher("A400088");
+        Matcher matcher = pattern.matcher(pin);
         boolean result = matcher.matches();
-        System.out.println(result);
+//        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println("Matcing pincode using regex:");
+        System.out.println("---------------------------");
+        String p1 = "400088";
+        System.out.println(p1 + " ==> " +IsPinCodeMatch(p1));
+
+        String p2 = "A400088";
+        System.out.println(p2 + " ==> " +IsPinCodeMatch(p2));
     }
 }
