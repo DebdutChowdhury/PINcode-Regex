0312395961592472b04aa08e1d8446fe0c196f21
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-30 22:48:27 +0530

    Added: space is valid in the pincode

--- PinCodeRegex.java
+++ PinCodeRegex.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class PinCodeRegex {
     public static boolean IsPinCodeMatch(String pin){
-        Pattern pattern = Pattern.compile("^[0-9]{6}");    // my pincode regex here 
+        Pattern pattern = Pattern.compile("^[1-9]{1}[0-9]{2}\s{0,1}[0-9]{3}$");    // my pincode regex here 
         Matcher matcher = pattern.matcher(pin);
         boolean result = matcher.matches();
 //        System.out.println(result);
@@ -18,7 +18,10 @@ public class PinCodeRegex {
         String p2 = "A400088";                                        // in the beginning of pincode letter or syamol not allow
         System.out.println(p2 + " ==> " +IsPinCodeMatch(p2));
 
-	 String p3 = "400088B";					     // in the end of pincode letter or symbol not allow 
+	String p3 = "400088B";					     // in the end of pincode letter or symbol not allow 
         System.out.println(p3+ " ==> "+IsPinCodeMatch(p3));
+
+	String p4 = "400 888";         				    // in the middle of pincode space is valid
+        System.out.println(p4 +" ==> "+ IsPinCodeMatch(p4));
     }
 }
